be58d056af22218df77ec1b7a5603cdd5ab971fd
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-11-27 13:10:38 +0100

    fatto server.js per inviare email, ancora da fixare

---
+++ src/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.post('/contact', async(req,res) => {
+    
+    const {email , subject, message} = req.body;
+
+    try{
+        const transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'nale2002pers@gmail.com',
+                pass: 'Sicurissima123!',
+            }
+        });
+        
+        const mailOptions = {
+            from: email,
+            to: 'nale2002pers@gmail.com',
+            subject: subject,
+            message: message,
+        };
+
+        await transporter.sendMail(mailOptions);
+        res.status(200).send("Messaggio inviato con successo!");
+
+    }   catch (error){
+        console.error("Errore durante l'invio del messaggio",error);
+        res.status(500).send("Errore durante l'invio del messaggio");
+    }   
+
+    app.listen(PORT, () => {
+        console.log('server in esecuzione su https:localhost:${PORT}');
+    });
+
+});
