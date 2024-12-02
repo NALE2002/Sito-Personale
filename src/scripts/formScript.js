89c0e3813e9adb94fb0a52df848bc840b3d3a2e4
Neagu Alexandru <neagu.alex0709@gmail.com>
2024-12-02 15:29:39 +0100

    modificando formScript.js

--- src/scripts/formScript.js
+++ src/scripts/formScript.js
@@ -1,28 +1,30 @@
 const form = document.querySelector("form");
 const submitButton = document.getElementById("submitButton");
 const submitIcon = document.getElementById("submitIcon");
+const loadingIcon = document.getElementById("loadingIcon");
 const successIcon = document.getElementById("successIcon");
 
 form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     submitButton.disabled = true;
-
+    submitIcon.classList.add("hidden");
+    loadingIcon.classList.remove("hidden");
 
     try {
         const res = await fetch(form.action, {
             method: form.method,
             body: new FormData(form),
         });
-
+        
         if(res.ok){
-            submitIcon.classList.add("hidden");
+            loadingIcon.classList.add("hidden");
             successIcon.classList.remove("hidden");
             submitButton.classList.add("bg-green-500");
         } else {
             alert("errore durante l'invio del messaggio");
         }
     } catch(error){
-        console.error("errore durante l'invio del messaggio ANNODAMACROP")
+        console.error("errore durante l'invio del messaggio")
     }
 });
